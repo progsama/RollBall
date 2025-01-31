ee105ee63236ecffd2a86efed14e94cfc989ec3a
progsama <imtiaznasif@gmail.com>
2025-01-30 20:40:21 -0800

    Jump coded in
    Added necessary code so that jump can be done using the sphere and it can only be done once so that multiple jumps dont make the sphere go flying

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -4,9 +4,21 @@ public class BallController : MonoBehaviour
 {
     [SerializeField] private Rigidbody sphereRigidbody;
     [SerializeField] private float ballSpeed = 2f;
+    [SerializeField] private bool jumpKey = false;
+
     public void MoveBall(Vector2 input)
     {
         Vector3 inputXZPlane = new Vector3(input.x, 0, input.y);
         sphereRigidbody.AddForce(inputXZPlane * ballSpeed);
+        if (Input.GetKeyDown(KeyCode.Space) && sphereRigidbody.position.y == 0.5)
+        {
+            jumpKey = true;
+        }
+        if (jumpKey == true)
+        {
+            jumpKey = false;
+            sphereRigidbody.AddForce(Vector3.up * ballSpeed, ForceMode.VelocityChange);
+            
+        }
     }
 }
