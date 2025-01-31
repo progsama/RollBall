7a054d8da29cbf239688bf396995ab7420f2f995
progsama <imtiaznasif@gmail.com>
2025-01-30 18:42:38 -0800

    Ball Movement; Camera Shift
    Changed The camera to provide a better overview angle of the plane and what goes on with the ball. Included movement with the ball so that it moves with definite legacy key presses.

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    public Rigidbody sphereRigidbody;
+    public float ballSpeed = 2f;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+
+    void Update()
+    {
+        Vector2 inputVector = Vector2.zero;
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputVector += Vector2.up;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputVector += Vector2.down;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector += Vector2.left;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputVector += Vector2.right;
+        }
+        Vector3 inputXZPlane = new Vector3(inputVector.x, 0, inputVector.y);
+        sphereRigidbody.AddForce(inputXZPlane);
+        Debug.Log("Resultant Vector: " + inputVector);
+        Debug.Log("Resultant 3D Vector: " + inputXZPlane);
+    }
+}
